ef161f331d2087991973f6f325d1e00e50c8b795
ibaddkhan <ibadurrehman718@gmail.com>
2024-02-23 16:41:29 +0500

    updated get request

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
- 
-app.use(cors())
-let port =3000
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+app.use(cors());
+app.use(express.json()); // Use this middleware to parse JSON bodies
+
+let port =  3000;
 require("dotenv").config();
 let arr = [{
   title: "world",
   ID: Date.now()
-},
-{
+}, {
   title: "world2",
   ID: Date.now()
-},
-{
+}, {
   title: "world3",
   ID: Date.now()
 }, {
@@ -22,7 +22,8 @@ let arr = [{
 }, {
   title: "world5",
   ID: Date.now()
-}]
+}];
+
 app.get("/", (req, res) => {
   res.send(arr);
 });
@@ -30,8 +31,19 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.send(`User Login<h1>${process.env.USERNAMESFORREPO}</h1>`);
 });
+
 app.get("/home", (req, res) => {
   res.send("Men  home ");
+});
+
+// POST route to add a new item
+app.post("/add", (req, res) => {
+  const newItem = {
+    title: req.body.title,
+    ID: Date.now() // Generate a new ID for the item
+  };
+  arr.push(newItem);
+  res.status(201).send(newItem); // Send back the newly added item
 });
 
 app.listen(port, () => {
